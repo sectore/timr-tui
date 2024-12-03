7053c65250af647a23d8fed7168793bfa74e0a9d
jk <47693+sectore@users.noreply.github.com>
2024-12-03 11:06:17 +0100

    StatefulWidget: AppWidget, CountdownWidget

--- src/app.rs
+++ src/app.rs
@@ -1,11 +1,15 @@
 use crate::{
     clock::{self, Clock},
     constants::TICK_VALUE_MS,
-    events::{Event, Events},
+    events::{Event, EventHandler, Events},
     terminal::Terminal,
     utils::center,
     widgets::{
-        countdown::Countdown, footer::Footer, header::Header, pomodoro::Pomodoro, timer::Timer,
+        countdown::{Countdown, CountdownWidget},
+        footer::Footer,
+        header::Header,
+        pomodoro::Pomodoro,
+        timer::Timer,
     },
 };
 use color_eyre::Result;
@@ -13,7 +17,7 @@ use ratatui::{
     buffer::Buffer,
     crossterm::event::{KeyCode, KeyEvent},
     layout::{Constraint, Layout, Rect},
-    widgets::{Block, Widget},
+    widgets::{Block, StatefulWidget, Widget},
 };
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -34,7 +38,7 @@ pub struct App {
     content: Content,
     mode: Mode,
     show_menu: bool,
-    clock_countdown: Clock<clock::Countdown>,
+    countdown: Countdown,
     clock_timer: Clock<clock::Timer>,
 }
 
@@ -44,9 +48,12 @@ impl Default for App {
             mode: Mode::Running,
             content: Content::Countdown,
             show_menu: false,
-            clock_countdown: Clock::<clock::Countdown>::new(
-                10 * 60 * 1000, /* 10min in milliseconds */
-                TICK_VALUE_MS,
+            countdown: Countdown::new(
+                "Countdown".into(),
+                Clock::<clock::Countdown>::new(
+                    10 * 60 * 1000, /* 10min in milliseconds */
+                    TICK_VALUE_MS,
+                ),
             ),
             clock_timer: Clock::<clock::Timer>::new(0, TICK_VALUE_MS),
         }
@@ -61,8 +68,11 @@ impl App {
     pub async fn run(&mut self, mut terminal: Terminal, mut events: Events) -> Result<()> {
         while self.is_running() {
             if let Some(event) = events.next().await {
+                if let Content::Countdown = self.content {
+                    self.countdown.update(event.clone())
+                };
                 match event {
-                    Event::Render | Event::Resize(_, _) => {
+                    Event::Render | Event::Resize => {
                         self.draw(&mut terminal)?;
                     }
                     Event::Tick => {
@@ -93,64 +103,61 @@ impl App {
         };
     }
 
-    fn draw(&self, terminal: &mut Terminal) -> Result<()> {
+    fn draw(&mut self, terminal: &mut Terminal) -> Result<()> {
         terminal.draw(|frame| {
-            frame.render_widget(self, frame.area());
+            frame.render_stateful_widget(AppWidget, frame.area(), self);
         })?;
         Ok(())
     }
 
-    fn render_content(&self, area: Rect, buf: &mut Buffer) {
-        // center content
-        let area = center(area, Constraint::Length(50), Constraint::Length(2));
-        match self.content {
-            Content::Timer => {
-                Timer::new("Timer".into(), self.clock_timer.clone()).render(area, buf)
-            }
-            Content::Countdown => {
-                Countdown::new("Countdown".into(), self.clock_countdown.clone()).render(area, buf)
-            }
-            Content::Pomodoro => Pomodoro::new("Pomodoro".into()).render(area, buf),
-        };
-    }
-
     fn reset(&mut self) {
-        match self.content {
-            Content::Timer => self.clock_timer.reset(),
-            Content::Countdown => self.clock_countdown.reset(),
-            _ => {}
+        if let Content::Timer = self.content {
+            self.clock_timer.reset()
         };
     }
 
     fn toggle(&mut self) {
-        match self.content {
-            Content::Timer => self.clock_timer.toggle_pause(),
-            Content::Countdown => self.clock_countdown.toggle_pause(),
-            _ => {}
+        if self.content == Content::Timer {
+            self.clock_timer.toggle_pause()
         };
     }
 
     fn tick(&mut self) {
-        match self.content {
-            Content::Timer => self.clock_timer.tick(),
-            Content::Countdown => self.clock_countdown.tick(),
-            _ => {}
+        if self.content == Content::Timer {
+            self.clock_timer.tick()
         };
     }
 }
 
-impl Widget for &App {
-    fn render(self, area: Rect, buf: &mut Buffer) {
+struct AppWidget;
+
+impl AppWidget {
+    fn render_content(&self, area: Rect, buf: &mut Buffer, state: &mut App) {
+        // center content
+        let area = center(area, Constraint::Length(50), Constraint::Length(2));
+        match state.content {
+            Content::Timer => {
+                Timer::new("Timer".into(), state.clock_timer.clone()).render(area, buf)
+            }
+            Content::Countdown => CountdownWidget.render(area, buf, &mut state.countdown),
+            Content::Pomodoro => Pomodoro::new("Pomodoro".into()).render(area, buf),
+        };
+    }
+}
+
+impl StatefulWidget for AppWidget {
+    type State = App;
+    fn render(self, area: Rect, buf: &mut Buffer, state: &mut Self::State) {
         let vertical = Layout::vertical([
             Constraint::Length(1),
             Constraint::Fill(0),
-            Constraint::Length(if self.show_menu { 2 } else { 1 }),
+            Constraint::Length(if state.show_menu { 2 } else { 1 }),
         ]);
         let [v0, v1, v4] = vertical.areas(area);
 
         Block::new().render(area, buf);
         Header::new(true).render(v0, buf);
-        self.render_content(v1, buf);
-        Footer::new(self.show_menu, self.content).render(v4, buf);
+        self.render_content(v1, buf, state);
+        Footer::new(state.show_menu, state.content).render(v4, buf);
     }
 }
