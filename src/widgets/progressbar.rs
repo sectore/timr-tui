d06107a5f88919ec252cff2b1f3ffc898eaea57b
jk <47693+sectore@users.noreply.github.com>
2024-12-25 12:05:26 +0100

    label

--- src/widgets/progressbar.rs
+++ src/widgets/progressbar.rs
@@ -19,13 +19,21 @@ impl Progressbar {
 
 impl Widget for Progressbar {
     fn render(self, area: Rect, buf: &mut Buffer) {
-        let [h0, h1] =
+        let label = Span::raw(format!(" {}% ", self.percentage));
+        let [h0, area] = Layout::horizontal([
+            Constraint::Length(label.width() as u16),
+            Constraint::Percentage(100),
+        ])
+        .areas(area);
+        let [h1, h2] =
             Layout::horizontal([Constraint::Percentage(self.percentage), Constraint::Fill(0)])
                 .areas(area);
 
+        // label
+        label.render(h0, buf);
         // done
-        Span::from(line::THICK_HORIZONTAL.repeat(h0.width as usize)).render(h0, buf);
+        Span::from(line::THICK_HORIZONTAL.repeat(h1.width as usize)).render(h1, buf);
         // rest
-        Span::from(line::HORIZONTAL.repeat(h1.width as usize)).render(h1, buf);
+        Span::from(line::HORIZONTAL.repeat(h2.width as usize)).render(h2, buf);
     }
 }
