5eb831457bc035c92f10369050b6854cda151d64
jk <47693+sectore@users.noreply.github.com>
2024-12-27 18:34:56 +0100

    progressbar: remove label

--- src/widgets/progressbar.rs
+++ src/widgets/progressbar.rs
@@ -19,18 +19,9 @@ impl Progressbar {
 
 impl Widget for Progressbar {
     fn render(self, area: Rect, buf: &mut Buffer) {
-        let label = Span::raw(format!(" {}% ", self.percentage));
-        let [h0, area] = Layout::horizontal([
-            Constraint::Length(label.width() as u16),
-            Constraint::Percentage(100),
-        ])
-        .areas(area);
         let [h1, h2] =
             Layout::horizontal([Constraint::Percentage(self.percentage), Constraint::Fill(0)])
                 .areas(area);
-
-        // label
-        label.render(h0, buf);
         // done
         Span::from(line::THICK_HORIZONTAL.repeat(h1.width as usize)).render(h1, buf);
         // rest
