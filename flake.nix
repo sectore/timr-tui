0281e89e04c36b11ba4a5990d1f7cee83b043e0a
jk <47693+sectore@users.noreply.github.com>
2025-06-28 11:26:36 +0200

    nix flake update

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
         {
           file = ./rust-toolchain.toml;
           # sha256 = nixpkgs.lib.fakeSha256;
-          sha256 = "sha256-KUm16pHj+cRedf8vxs/Hd2YWxpOrWZ7UOrwhILdSJBU=";
+          sha256 = "sha256-Qxt8XAuaUR2OMdKbN4u8dBJOhSHxS+uS06Wl9+flVEk=";
         };
 
       craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
