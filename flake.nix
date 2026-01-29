74aac46e77bcd999e35e04d19ed48264926b3366
jk <47693+sectore@users.noreply.github.com>
2026-01-29 17:38:52 +0100

    Rust 1.93.0
    https://blog.rust-lang.org/2026/01/22/Rust-1.93.0/

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
         {
           file = ./rust-toolchain.toml;
           # sha256 = nixpkgs.lib.fakeSha256;
-          sha256 = "sha256-sqSWJDUxc+zaz1nBWMAJKTAGBuGWP25GCftIOlCEAtA=";
+          sha256 = "sha256-vra6TkHITpwRyA5oBKAHSX0Mi6CBDNQD+ryPSpxFsfg=";
         };
 
       craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
