e9c8ea2625f68cb0b8eda1c92faf2f70cbd1e804
jk <47693+sectore@users.noreply.github.com>
2026-01-29 20:31:57 +0100

    update TODOs for deprecated keys

--- src/widgets/pomodoro.rs
+++ src/widgets/pomodoro.rs
@@ -188,9 +188,8 @@ impl TuiEventHandler for PomodoroState {
             },
             // default mode
             TuiEvent::Crossterm(CrosstermEvent::Key(key)) => match key.code {
-                // TODO: Deprecate 's' in next major release
                 // Toggle run/pause
-                KeyCode::Char('s') | KeyCode::Char(' ') => {
+                KeyCode::Char(' ') | KeyCode::Char('s') /* TODO: deprecated, remove it in next version */ => {
                     self.get_clock_mut().toggle_pause();
                 }
                 // Enter edit mode
--- src/widgets/timer.rs
+++ src/widgets/timer.rs
@@ -78,9 +78,8 @@ impl TuiEventHandler for TimerState {
             },
             // default mode
             TuiEvent::Crossterm(CrosstermEvent::Key(key)) => match key.code {
-                // TODO: Deprecate 's' in next major release
                 // Toggle run/pause
-                KeyCode::Char('s') | KeyCode::Char(' ') => {
+                KeyCode::Char(' ') | KeyCode::Char('s') /* TODO: deprecated, remove it in next version */ => {
                     self.clock.toggle_pause();
                 }
                 // reset clock
