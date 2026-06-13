6545b80bcfb52aabb980145fddaf23d3e6f12b41
jk <47693+sectore@users.noreply.github.com>
2026-06-13 20:21:03 +0200

    fix(test): parse duration, override state args

--- src/widgets/clock_widget_test.rs
+++ src/widgets/clock_widget_test.rs
@@ -1,6 +1,6 @@
 use crate::{
     common::Style,
-    duration::{MAX_DURATION, ONE_DAY, ONE_HOUR, ONE_MINUTE, ONE_SECOND, ONE_YEAR},
+    duration::{MAX_DURATION, parse_duration, parse_long_duration},
     widgets::{
         clock::{ClockState, ClockStateArgs, ClockWidget, Timer},
         clock_elements::DIGIT_HEIGHT,
@@ -15,14 +15,22 @@ fn w() -> ClockWidget<Timer> {
     ClockWidget::new(Style::default(), false)
 }
 
-fn st(current_value: Duration) -> ClockState<Timer> {
-    ClockState::<Timer>::new(ClockStateArgs {
-        initial_value: current_value,
-        current_value,
+fn args() -> ClockStateArgs {
+    ClockStateArgs {
+        initial_value: Duration::from_hours(21),
+        current_value: Duration::from_mins(21),
         tick_value: Duration::from_millis(100),
         with_decis: false,
         app_tx: None,
-    })
+    }
+}
+
+fn st_with_args(args: ClockStateArgs) -> ClockState<Timer> {
+    ClockState::<Timer>::new(args)
+}
+
+fn _st() -> ClockState<Timer> {
+    st_with_args(args())
 }
 
 fn terminal(w: ClockWidget<Timer>, st: ClockState<Timer>) -> Terminal<TestBackend> {
@@ -38,265 +46,250 @@ fn terminal(w: ClockWidget<Timer>, st: ClockState<Timer>) -> Terminal<TestBacken
 
 #[test]
 fn test_clock_yyyydddhhmmss() {
-    let t = terminal(w(), st(MAX_DURATION));
+    let st = st_with_args(ClockStateArgs {
+        current_value: MAX_DURATION,
+        ..args()
+    });
+    let t = terminal(w(), st);
     assert_snapshot!("clock_yyyydddhhmmss", t.backend());
 }
 
 #[test]
 fn test_clock_yyyyddhhmmss() {
-    let t = terminal(
-        w(),
-        st(ONE_YEAR.saturating_mul(1000)
-            + ONE_DAY.saturating_mul(10)
-            + ONE_HOUR.saturating_mul(14)
-            + ONE_MINUTE.saturating_mul(30)
-            + ONE_SECOND.saturating_mul(5)),
-    );
+    let current_value = parse_long_duration("1000y 10d 14:30:05").unwrap();
+    let st = st_with_args(ClockStateArgs {
+        current_value,
+        ..args()
+    });
+    let t = terminal(w(), st);
     assert_snapshot!("clock_yyyyddhhmmss", t.backend());
 }
 
 #[test]
 fn test_clock_yyyydhhmmss() {
-    let t = terminal(
-        w(),
-        st(ONE_YEAR.saturating_mul(1000)
-            + ONE_DAY
-            + ONE_HOUR.saturating_mul(14)
-            + ONE_MINUTE.saturating_mul(30)
-            + ONE_SECOND.saturating_mul(5)),
-    );
+    let current_value = parse_long_duration("1000y 1d 14:30:05").unwrap();
+    let st = st_with_args(ClockStateArgs {
+        current_value,
+        ..args()
+    });
+    let t = terminal(w(), st);
     assert_snapshot!("clock_yyyydhhmmss", t.backend());
 }
 
 #[test]
 fn test_clock_yyydddhhmmss() {
-    let t = terminal(
-        w(),
-        st(ONE_YEAR.saturating_mul(100)
-            + ONE_DAY.saturating_mul(100)
-            + ONE_HOUR.saturating_mul(14)
-            + ONE_MINUTE.saturating_mul(30)
-            + ONE_SECOND.saturating_mul(5)),
-    );
+    let current_value = parse_long_duration("100y 100d 14:30:05").unwrap();
+    let st = st_with_args(ClockStateArgs {
+        current_value,
+        ..args()
+    });
+    let t = terminal(w(), st);
     assert_snapshot!("clock_yyydddhhmmss", t.backend());
 }
 
 #[test]
 fn test_clock_yyyddhhmmss() {
-    let t = terminal(
-        w(),
-        st(ONE_YEAR.saturating_mul(100)
-            + ONE_DAY.saturating_mul(10)
-            + ONE_HOUR.saturating_mul(14)
-            + ONE_MINUTE.saturating_mul(30)
-            + ONE_SECOND.saturating_mul(5)),
-    );
+    let current_value = parse_long_duration("100y 10d 14:30:05").unwrap();
+    let st = st_with_args(ClockStateArgs {
+        current_value,
+        ..args()
+    });
+    let t = terminal(w(), st);
     assert_snapshot!("clock_yyyddhhmmss", t.backend());
 }
 
 #[test]
 fn test_clock_yyydhhmmss() {
-    let t = terminal(
-        w(),
-        st(ONE_YEAR.saturating_mul(100)
-            + ONE_DAY
-            + ONE_HOUR.saturating_mul(14)
-            + ONE_MINUTE.saturating_mul(30)
-            + ONE_SECOND.saturating_mul(5)),
-    );
+    let current_value = parse_long_duration("100y 1d 14:30:05").unwrap();
+    let st = st_with_args(ClockStateArgs {
+        current_value,
+        ..args()
+    });
+    let t = terminal(w(), st);
     assert_snapshot!("clock_yyydhhmmss", t.backend());
 }
 
 #[test]
 fn test_clock_yydddhhmmss() {
-    let t = terminal(
-        w(),
-        st(ONE_YEAR.saturating_mul(10)
-            + ONE_DAY.saturating_mul(100)
-            + ONE_HOUR.saturating_mul(14)
-            + ONE_MINUTE.saturating_mul(30)
-            + ONE_SECOND.saturating_mul(5)),
-    );
+    let current_value = parse_long_duration("10y 100d 14:30:05").unwrap();
+    let st = st_with_args(ClockStateArgs {
+        current_value,
+        ..args()
+    });
+    let t = terminal(w(), st);
     assert_snapshot!("clock_yydddhhmmss", t.backend());
 }
 
 #[test]
 fn test_clock_yyddhhmmss() {
-    let t = terminal(
-        w(),
-        st(ONE_YEAR.saturating_mul(10)
-            + ONE_DAY.saturating_mul(10)
-            + ONE_HOUR.saturating_mul(14)
-            + ONE_MINUTE.saturating_mul(30)
-            + ONE_SECOND.saturating_mul(5)),
-    );
+    let current_value = parse_long_duration("10y 10d 14:30:05").unwrap();
+    let st = st_with_args(ClockStateArgs {
+        current_value,
+        ..args()
+    });
+    let t = terminal(w(), st);
     assert_snapshot!("clock_yyddhhmmss", t.backend());
 }
 
 #[test]
 fn test_clock_yydhhmmss() {
-    let t = terminal(
-        w(),
-        st(ONE_YEAR.saturating_mul(10)
-            + ONE_DAY
-            + ONE_HOUR.saturating_mul(14)
-            + ONE_MINUTE.saturating_mul(30)
-            + ONE_SECOND.saturating_mul(5)),
-    );
+    let current_value = parse_long_duration("10y 1d 14:30:05").unwrap();
+    let st = st_with_args(ClockStateArgs {
+        current_value,
+        ..args()
+    });
+    let t = terminal(w(), st);
     assert_snapshot!("clock_yydhhmmss", t.backend());
 }
 
 #[test]
 fn test_clock_ydddhhmmss() {
-    let t = terminal(
-        w(),
-        st(ONE_YEAR
-            + ONE_DAY.saturating_mul(100)
-            + ONE_HOUR.saturating_mul(14)
-            + ONE_MINUTE.saturating_mul(30)
-            + ONE_SECOND.saturating_mul(5)),
-    );
+    let current_value = parse_long_duration("1y 100d 14:30:05").unwrap();
+    let st = st_with_args(ClockStateArgs {
+        current_value,
+        ..args()
+    });
+    let t = terminal(w(), st);
     assert_snapshot!("clock_ydddhhmmss", t.backend());
 }
 
 #[test]
 fn test_clock_yddhhmmss() {
-    let t = terminal(
-        w(),
-        st(ONE_YEAR
-            + ONE_DAY.saturating_mul(10)
-            + ONE_HOUR.saturating_mul(14)
-            + ONE_MINUTE.saturating_mul(30)
-            + ONE_SECOND.saturating_mul(5)),
-    );
+    let current_value = parse_long_duration("1y 10d 14:30:05").unwrap();
+    let st = st_with_args(ClockStateArgs {
+        current_value,
+        ..args()
+    });
+    let t = terminal(w(), st);
     assert_snapshot!("clock_yddhhmmss", t.backend());
 }
 
 #[test]
 fn test_clock_ydhhmmss() {
-    let t = terminal(
-        w(),
-        st(ONE_YEAR
-            + ONE_DAY
-            + ONE_HOUR.saturating_mul(14)
-            + ONE_MINUTE.saturating_mul(30)
-            + ONE_SECOND.saturating_mul(5)),
-    );
+    let current_value = parse_long_duration("1y 1d 14:30:05").unwrap();
+    let st = st_with_args(ClockStateArgs {
+        current_value,
+        ..args()
+    });
+    let t = terminal(w(), st);
     assert_snapshot!("clock_ydhhmmss", t.backend());
 }
 
 #[test]
 fn test_clock_dddhhmmss() {
-    let t = terminal(
-        w(),
-        st(ONE_DAY.saturating_mul(100)
-            + ONE_HOUR.saturating_mul(14)
-            + ONE_MINUTE.saturating_mul(30)
-            + ONE_SECOND.saturating_mul(5)),
-    );
+    let current_value = parse_long_duration("100d 14:30:05").unwrap();
+    let st = st_with_args(ClockStateArgs {
+        current_value,
+        ..args()
+    });
+    let t = terminal(w(), st);
     assert_snapshot!("clock_dddhhmmss", t.backend());
 }
 
 #[test]
 fn test_clock_ddhhmmss() {
-    let t = terminal(
-        w(),
-        st(ONE_DAY.saturating_mul(10)
-            + ONE_HOUR.saturating_mul(14)
-            + ONE_MINUTE.saturating_mul(30)
-            + ONE_SECOND.saturating_mul(5)),
-    );
+    let current_value = parse_long_duration("10d 14:30:05").unwrap();
+    let st = st_with_args(ClockStateArgs {
+        current_value,
+        ..args()
+    });
+    let t = terminal(w(), st);
     assert_snapshot!("clock_ddhhmmss", t.backend());
 }
 
 #[test]
 fn test_clock_dhhmmss() {
-    let t = terminal(
-        w(),
-        st(ONE_DAY
-            + ONE_HOUR.saturating_mul(14)
-            + ONE_MINUTE.saturating_mul(30)
-            + ONE_SECOND.saturating_mul(5)),
-    );
+    let current_value = parse_long_duration("1d 14:30:05").unwrap();
+    let st = st_with_args(ClockStateArgs {
+        current_value,
+        ..args()
+    });
+    let t = terminal(w(), st);
     assert_snapshot!("clock_dhhmmss", t.backend());
 }
 
 #[test]
 fn test_clock_hhmmss() {
-    let t = terminal(
-        w(),
-        st(ONE_HOUR.saturating_mul(14)
-            + ONE_MINUTE.saturating_mul(30)
-            + ONE_SECOND.saturating_mul(5)),
-    );
+    let current_value = parse_duration("14:30:05").unwrap();
+    let st = st_with_args(ClockStateArgs {
+        current_value,
+        ..args()
+    });
+    let t = terminal(w(), st);
     assert_snapshot!("clock_hhmmss", t.backend());
 }
 
 #[test]
 fn test_clock_hmmss() {
-    let t = terminal(
-        w(),
-        st(ONE_HOUR + ONE_MINUTE.saturating_mul(30) + ONE_SECOND.saturating_mul(5)),
-    );
+    let current_value = parse_duration("1:30:05").unwrap();
+    let st = st_with_args(ClockStateArgs {
+        current_value,
+        ..args()
+    });
+    let t = terminal(w(), st);
     assert_snapshot!("clock_hmmss", t.backend());
 }
 
 #[test]
 fn test_clock_mmss() {
-    let t = terminal(
-        w(),
-        st(ONE_MINUTE.saturating_mul(30) + ONE_SECOND.saturating_mul(5)),
-    );
+    let current_value = parse_duration("30:05").unwrap();
+    let st = st_with_args(ClockStateArgs {
+        current_value,
+        ..args()
+    });
+    let t = terminal(w(), st);
     assert_snapshot!("clock_mmss", t.backend());
 }
 
 #[test]
 fn test_clock_mss() {
-    let t = terminal(
-        w(),
-        st(ONE_MINUTE.saturating_mul(5) + ONE_SECOND.saturating_mul(42)),
-    );
+    let current_value = parse_duration("5:42").unwrap();
+    let st = st_with_args(ClockStateArgs {
+        current_value,
+        ..args()
+    });
+    let t = terminal(w(), st);
     assert_snapshot!("clock_mss", t.backend());
 }
 
 #[test]
 fn test_clock_ss() {
-    let t = terminal(w(), st(ONE_SECOND.saturating_mul(42)));
+    let st = st_with_args(ClockStateArgs {
+        current_value: Duration::from_secs(42),
+        ..args()
+    });
+    let t = terminal(w(), st);
     assert_snapshot!("clock_ss", t.backend());
 }
 
 #[test]
 fn test_clock_s() {
-    let t = terminal(w(), st(ONE_SECOND.saturating_mul(7)));
+    let st = st_with_args(ClockStateArgs {
+        current_value: Duration::from_secs(7),
+        ..args()
+    });
+    let t = terminal(w(), st);
     assert_snapshot!("clock_s", t.backend());
 }
 
 #[test]
 fn test_clock_decis() {
-    let t = terminal(
-        w(),
-        ClockState::<Timer>::new(ClockStateArgs {
-            initial_value: ONE_SECOND.saturating_mul(7),
-            current_value: ONE_SECOND.saturating_mul(7),
-            tick_value: Duration::from_millis(100),
-            with_decis: true,
-            app_tx: None,
-        }),
-    );
+    let st = st_with_args(ClockStateArgs {
+        current_value: Duration::from_secs(7),
+        with_decis: true,
+        ..args()
+    });
+    let t = terminal(w(), st);
     assert_snapshot!("clock_decis", t.backend());
 }
 
 #[test]
 fn test_clock_style() {
-    let t = terminal(
-        ClockWidget::new(Style::Braille, false),
-        ClockState::<Timer>::new(ClockStateArgs {
-            initial_value: MAX_DURATION,
-            current_value: MAX_DURATION,
-            tick_value: Duration::from_millis(100),
-            with_decis: true,
-            app_tx: None,
-        }),
-    );
+    let st = st_with_args(ClockStateArgs {
+        current_value: MAX_DURATION,
+        with_decis: true,
+        ..args()
+    });
+    let t = terminal(ClockWidget::new(Style::Braille, false), st);
     assert_snapshot!("clock_style", t.backend());
 }
